4bafd900502129366bbe7e18503b964f579478de
minibox24 <minibox724@gmail.com>
2021-09-01 22:26:42 +0900

    ✨ 채널 정보 라우트

--- app.py
+++ app.py
@@ -22,6 +22,34 @@ async def exist_channel(id_: int):
     return {"exist": exist}
 
 
+@app.get("/info/{id_}")
+async def channel_info(id_: int):
+    channel = await ToonChannel.get(id=id_)
+    all_ = await ToonData.filter(channel_id=id_).count()
+
+    contributors = {}
+
+    for user_id in [
+        d["user_id"] for d in await ToonData.filter(channel_id=id_).values("user_id")
+    ]:
+        if user_id not in contributors:
+            user = await ToonUser.get(id=user_id)
+            contributors[user_id] = {
+                "name": user.name,
+                "avatar": user.avatar,
+                "count": 0,
+            }
+        contributors[user_id]["count"] += 1
+
+    return {
+        "name": channel.name,
+        "all": all_,
+        "contributors": sorted(
+            list(contributors.values()), key=lambda x: x["count"], reverse=True
+        ),
+    }
+
+
 @app.get("/images/{id_}")
 async def get_images(id_: int):
     images = await ToonData.filter(channel_id=id_).order_by("created_at").values("url")
